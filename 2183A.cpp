51f28b1744f0e7ac43ba918b357dd62264ccdb9f
Priyanshu Kumar <priyanshu19jul@gmail.com>
2026-01-07 21:36:06 +0530

    questions

--- 2183A.cpp
+++ 2183A.cpp
@@ -1,15 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std ;
 #define int long long
-signed main(){
-   int t;
+signed main() {
+    int t;
     if (cin >> t) {
         while (t--) {
             int n;
             cin >> n;
-            
+            int a[n];
+            for (int i = 0; i < n; i++) {
+                cin >> a[i];
+            }
+            if (a[0] == 1 || a[n - 1] == 1) {
+                cout << "Alice" << "\n";
+            } 
+            else {
+                cout << "Bob" << "\n";
+            }
         }
     }
     return 0;
-    
 }
